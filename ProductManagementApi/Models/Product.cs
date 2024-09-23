2fe97f1eecd1fde2004bd42260dce708ee1f920d
LucasBalbueno <balbuenolucas04@gmail.com>
2024-09-23 10:21:00 -0300

    Criando models e configurando database

---
+++ ProductManagementApi/Models/Product.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public double? Price { get; set; }
+    [Required]
+    public int? Quantity { get; set; }
+}
