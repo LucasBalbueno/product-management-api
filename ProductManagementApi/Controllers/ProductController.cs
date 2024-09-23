a3024ee527ebcf00985d1c3f312aa09e48086600
LucasBalbueno <balbuenolucas04@gmail.com>
2024-09-23 13:57:52 -0300

    Documentando API com Swagger

--- ProductManagementApi/Controllers/ProductController.cs
+++ ProductManagementApi/Controllers/ProductController.cs
@@ -15,7 +15,21 @@ namespace ProductManagementApi.Controllers
         _productRepo = productRepo;
     }
 
-        [HttpPost]
+    /// <summary>
+    /// Cria um novo produto
+    /// </summary>
+    /// <remarks>
+    /// O método POST cria um novo produto no banco de dados. O objeto produto deve ser passado no corpo da requisição.
+    /// Exemplo de objeto produto:
+    /// { "id": 1, "name": "NameExample", "price": 10, "quantity": 1 }
+    /// </remarks>
+    /// <param name="product">Objeto produto com suas informações de criação</param>
+    /// <returns>Retorna um objeto de product</returns>
+    /// <response code="201">Sucesso!</response>
+    /// <response code="500">Erro interno do servidor</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> CreateProduct(Product product) {
         try {
             var createdProduct = await _productRepo.AddProductAsync(product);
@@ -29,7 +43,18 @@ namespace ProductManagementApi.Controllers
         }
     }
 
+    /// <summary>
+    /// Obter todos os produtos presente no Banco de Dados
+    /// </summary>
+    /// <remarks>
+    /// Esse método GET retorna todos os produtos presentes no banco de dados. Não é necessário passar nenhum parâmetro.
+    /// </remarks>
+    /// <returns>Retorna toda a coleção de produtos atual</returns>
+    /// <response code="200">Sucesso!</response>
+    /// <response code="500">Erro interno do servidor</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetAllProducts() {
         try {
             var products = await _productRepo.GetAllProductsAsync();
@@ -43,7 +68,21 @@ namespace ProductManagementApi.Controllers
         }
     }
 
-        [HttpGet("{id}")]
+    /// <summary>
+    /// Obter um produto específico
+    /// </summary>
+    /// <remarks>
+    /// Esse método GET retorna um produto específico baseado no ID passado como parâmetro.
+    /// </remarks>
+    /// <param name="id">Identificador (ID) do produto a ser lido</param>
+    /// <returns>Retorna um produto específico baseado no ID passado como parâmetro</returns>
+    /// <response code="200">Sucesso!</response>
+    /// <response code="404">Produto não encontrado</response>
+    /// <response code="500">Erro interno do servidor</response>
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetProductById(int id) {
         try {
             var product = await _productRepo.GetProductByIdAsync(id);
@@ -63,7 +102,23 @@ namespace ProductManagementApi.Controllers
         }
     }
 
+    /// <summary>
+    /// Atualiza um produto específico
+    /// </summary>
+    /// <remarks>
+    /// O método PUT atualiza um produto específico no banco de dados. O objeto produto com suas informações de atualização deve ser passado no corpo da requisição, inclusive o ID do produto a ser atualizado.
+    /// Exemplo de objeto produto:
+    /// { "id": 1, "name": "NameAtualizado", "price": 50, "quantity": 5 }
+    /// </remarks>
+    /// <param name="productUpdated">Objeto produto com suas informações de atualização</param>
+    /// <returns>Não retorna nada</returns>
+    /// <response code="204">Sucesso!</response>
+    /// <response code="404">Produto não encontrado</response>
+    /// <response code="500">Erro interno do servidor</response>
     [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> UpdateProduct(Product productUpdated) {
         try {
             var product = await _productRepo.GetProductByIdAsync(productUpdated.Id);
@@ -84,7 +139,21 @@ namespace ProductManagementApi.Controllers
         }
     }
 
+    /// <summary>
+    /// Deleta um produto específico
+    /// </summary>
+    /// <remarks>
+    /// O método DELETE deleta um produto específico no banco de dados.
+    /// </remarks>
+    /// <param name="id">Identificador (ID) do produto a ser deletado</param>
+    /// <returns>Não retorna nada</returns>
+    /// <response code="204">Sucesso!</response>
+    /// <response code="404">Produto não encontrado</response>
+    /// <response code="500">Erro interno do servidor</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> DeleteProduct(int id) {
         try {
             var product = await _productRepo.GetProductByIdAsync(id);
--- ProductManagementApi/Program.cs
+++ ProductManagementApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using ProductManagementApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "ProductManagementApi",
+        Version = "v1",
+        Contact = new OpenApiContact {
+            Name = "Lucas Balbueno Vicêncio",
+            Email = "balbuenolucas04@gmail.com",
+            Url = new Uri("https://github.com/LucasBalbueno")
+        }
+    });
+
+    var xmlFile = "ProductManagementApi.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 
